25139cb1bab7142b87271139c31126db4ed64462
54ja <ja54@mahurangi.school.nz>
2022-06-08 12:00:25 +1200

    Fixed Ds store

--- index.php
+++ index.php
@@ -6,9 +6,36 @@
 
 <div class="jumbotron text-center"> 
 
+<script src="js/jquery.flexslider.js"></script>
+<script type="text/javascript">
+$(window).load(function() {
+ $('.flexslider').flexslider({
+	animation: "slide",
+        slideshowSpeed: 4000,
+        animationSpeed: 1000, 
+	controlNav: false
+	});
+});
+</script>
 
- 
+<div class="flexslider">
+<ul class="slides">
+<li><img src="images/fruit.jpg" alt="1" /></li>
+<li><img src="images/waterfall.jpg" alt="2" /></li>
+<li><img src="images/hourse-3.jpg" alt="3" /></li>
+<li><img src="images/spark.jpg" alt="4" /></li>
+</ul>
 </div>
+
+
+
+</div>
+
+<div class="h1">Southern Paprika</div>
+<p></p>
+<h8>Fresh capsicums straight from the crops with fresh delivery. Feel the aroma of chilled capsicums, the crunch of a ripe sweet paprika.</h8>
+<P></P>
+<h2>Bringing fresh food to our locals</h2> 
 
 <div class="about-section">
   <h2><u>Southern paprika</u></h2>
