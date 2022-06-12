d2cd9aa085d0fbe1cf66057ff715e8bf00fa9369
54ja <ja54@mahurangi.school.nz>
2022-06-13 09:05:59 +1200

    Version 3

--- index.php
+++ index.php
@@ -6,28 +6,6 @@
 
 <div class="jumbotron text-center"> 
 
-<script src="js/jquery.flexslider.js"></script>
-<script type="text/javascript">
-$(window).load(function() {
- $('.flexslider').flexslider({
-	animation: "slide",
-        slideshowSpeed: 4000,
-        animationSpeed: 1000, 
-	controlNav: false
-	});
-});
-</script>
-
-<div class="flexslider">
-<ul class="slides">
-<li><img src="images/fruit.jpg" alt="1" /></li>
-<li><img src="images/waterfall.jpg" alt="2" /></li>
-<li><img src="images/hourse-3.jpg" alt="3" /></li>
-<li><img src="images/spark.jpg" alt="4" /></li>
-</ul>
-</div>
-
-
 
 </div>
 
