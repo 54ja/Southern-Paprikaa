9828d4a3befc1b2a502a0b765e99abac5d0b1eec
54ja <ja54@mahurangi.school.nz>
2022-06-27 09:48:03 +1200

    v8 started the shop page

--- test.php
+++ test.php
@@ -15,7 +15,7 @@
 <div class="card">
 <img src="imagers/deer.jpg" alt="Photo of deer in forrest" style="width:100%">
       <div class="container">
-        <h2>The deer in the forrest</h2>
+        <h2>Capsicums</h2>
         <h4 class="title">By Bob Mullen</h4>
         <h5>Bob Mullen, a student in year 10 took this photo when he was in the South Island on holiday. While travelling through the bush, he came across it and managed to get a photo before it ran away.</h5>
       </div>
@@ -26,7 +26,7 @@
     <div class="card">
       <img src="imagers/fruit.jpg" alt="Painting of fruit" style="width:100%">
       <div class="container">
-        <h2>Summer's fruit</h2>
+        <h2>Avocardos</h2>
         <h4 class="title">By Aimee Tomlinson</h4>
         <h5>Aimee Tomlinson, a student in year 12 painted this art piece over the course of a term for her level 2 art portfolio. The piece consists of different fruits that are found in summer and brings out the vibrant colours that they have.</h5>
       </div>
@@ -37,7 +37,7 @@
     <div class="card">
       <img src="imagers/ladybug.jpg" alt="Macro picture of 2 lady bugs" style="width:100%">
       <div class="container">
-        <h2>Lady bug love</h2>
+        <h2>Cucumbers</h2>
         <h4 class="title">By Eve Wade</h4>
         <h5>Eve Wade, a student in year 9 took this photo over lock down in her backyard. Because of lock down everyone was forced to work from home. While trying to find something to captured, she found 2 lady bugs and quickly got this incredible photo.
  </h5>
