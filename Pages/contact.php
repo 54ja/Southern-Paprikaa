a200ab07916e3e055b7c439b3bac4143c8265bd0
54ja <78886190+54ja@users.noreply.github.com>
2022-09-13 23:09:14 +1200

    v20- fixed my contact page so that it has a map and displays more information.

--- Pages/contact.php
+++ Pages/contact.php
@@ -42,7 +42,8 @@ input[type=submit]:hover {
 
 form {
   width: 50%;
-  margin: 0 auto;
+  margin-left: 100px;
+  margin-top: 50px;
   padding: 10px;
   border: 1px solid #B0C4DE;
   border-radius: 2px;
@@ -50,12 +51,33 @@ form {
   float: left;
 }
 
+iframe {
+  margin-left: 100px;
+  margin-top: 50px;
+  margin-bottom: 150px;
+  padding: 10px;
+  border: 1px solid #B0C4DE;
+  border-radius: 2px;
+  background: #f4f4f4;
+  float : inherit;
+}
+
+@media (max-width:767px) {
+  iframe , form {
+    width: 75%;
+    padding-bottom:5px;
+    margin: none !important;
+    float: center;
+    margin-left: 50px;
+
+    
+  }
+}
+
 
 </style>
 <div class="bg">
-<div style="
-  background-color: #f2f2f2;">
-<div class="container col-sm-8">
+<div style="background-color: #f2f2f2;">
 <?php if(isset($_SESSION['messages'])){ 
 					?><h1><?php echo $_SESSION['messages']; ?></h1> <?php
 					unset($_SESSION['messages']); }?>
@@ -78,9 +100,10 @@ form {
 
   </form>
 
-<iframe class="col-sm-4" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12844.17349144028!2d174.6068876981735!3d-36.40814590662162!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d0cde0f62b19b8b%3A0xd35375d999ab338d!2sSouthern%20Paprika%20Limited!5e0!3m2!1sen!2snz!4v1662329849823!5m2!1sen!2snz" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12844.17349144028!2d174.6068876981735!3d-36.40814590662162!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d0cde0f62b19b8b%3A0xd35375d999ab338d!2sSouthern%20Paprika%20Limited!5e0!3m2!1sen!2snz!4v1662329849823!5m2!1sen!2snz" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </div>
 </div>
-</div>
+
 <?php include("../pages/footer.html"); ?>
+
         
